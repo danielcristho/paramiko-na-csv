e38dc944644c619e48c9bbed0c1c493a7555c6be
danielcristho <pepuhodaniel93.learn@gmail.com>
2023-01-23 02:51:39 +0700

    fix: if-else for multivendor

--- script-csv.py
+++ script-csv.py
@@ -8,13 +8,12 @@ from datetime import datetime
 conn_ssh = paramiko.SSHClient()
 conn_ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-#Create
+#Create script log
 create_log = open("logs.txt", "a")
 
 #Read from CSV file
 readcsv_file = open("list-devices.csv", "r")
 listcsv_device = csv.DictReader(readcsv_file, delimiter=",")
-
 
 for device in listcsv_device:
     try:
@@ -25,20 +24,35 @@ for device in listcsv_device:
             port = device['port'] if device['port'] else 22,
             look_for_keys=False, allow_agent=False
         )
-        print("****************************************************")
-        print(f"Succes login to {device['username']}")
-
         conn = conn_ssh.invoke_shell()
+        print("****************************************************")
+        print(f"Success login to {device['username']}")
 
-        conn.send("show ip int br | exclude unass\n")
-        conn_ssh.exec_command("ip add pr\n")
-        time.sleep(10)
-        output = conn.recv(65535).decode()
+        if device['vendor'] == "cisco":
+            conn.send("enable\n")
+            conn.send("admin\n")
+            conn.send("conf t\n")
+            conn.send("int lo0\n")
+            conn.send("ip add 1.1.1.1 255.255.255.255\n")
+            conn.send("no sh\n")
+            conn.send("do show ip int br | exc unass\n")
+            time.sleep(10)
+            output = conn.recv(65535).decode()
+
+        elif device['vendor'] == "mikrotik":
+            conn_ssh.exec_command("interface vlan add name=testing vlan-id=10 interface=ether4\n")
+            conn_ssh.exec_command("interface vlan print\n")
+            time.sleep(10)
+            output = conn.recv(65535).decode()
+
+        else:
+            print("Unknown vendor")
+
+        conn_ssh.close()
         file_backup = open(f"backup/{device['ip']}.cfg", "w")
         file_backup.write(output)
         file_backup.close()
 
-        conn_ssh.close()
 #Create error exception
     except paramiko.ssh_exception.AuthenticationException as message:
         print(f"{message} [{device['ip']}] ")
@@ -55,5 +69,3 @@ for device in listcsv_device:
         create_log.write(f"error, message: {message} {datetime.now()}\n")
 
 create_log.close()
-
-
